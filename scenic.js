1bb1050b72571876d2d9168aecd48237b8bfd512
HTMLProjectMaster <74315691+HTMLProjectMaster@users.noreply.github.com>
2021-03-23 13:22:40 +0100

    Uploading assets
    First commit, uploading assets.

---
+++ scenic.js
@@ -0,0 +1,7 @@
+/* load scripts in this file (link between engine and assets) */
+// dummy objects
+system.load('script', 'script-object-dummy-polygon', '../assets/objects/dummy-polygon.js', system.dom.head)
+system.load('script', 'script-object-dummy-move-particle', '../assets/objects/dummy-move-particle.js', system.dom.head)
+system.load('script', 'script-object-dummy-player', '../assets/objects/dummy-player.js', system.dom.head)
+system.load('script', 'script-object-dummy-cube', '../assets/objects/dummy-cube.js', system.dom.head)
+system.load('script', 'script-scene-dummy-scene', '../assets/scenes/dummy-scene.js', system.dom.head)
